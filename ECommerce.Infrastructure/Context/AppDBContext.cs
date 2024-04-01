b294511a3753afc2b9ac5043f5236b9c188197da
atalayozkn <atalayozkan16899@gmail.com>
2024-04-01 11:40:38 +0300

    Cart/CartItemCreated

--- ECommerce.Infrastructure/Context/AppDBContext.cs
+++ ECommerce.Infrastructure/Context/AppDBContext.cs
@@ -1,11 +1,5 @@
-﻿using Humanizer.Localisation;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ECommerce.Infrastructure.Context
 {
