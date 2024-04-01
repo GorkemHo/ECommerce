37afb4d7e4760899ecd871d18abaed5789ca52c0
DamlaGurel <damla.gureel@gmail.com>
2024-04-01 12:28:32 +0300

    appdbcontext classı oluşturuldu.

--- ECommerce.Infrastructure/Context/AppDBContext.cs
+++ ECommerce.Infrastructure/Context/AppDBContext.cs
@@ -8,10 +8,24 @@ namespace ECommerce.Infrastructure.Context
     {
         public AppDbContext() { }
 
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=DESKTOP-16FKK09; Database=ONLY7BlogProjectDB; Uid=sa; Pwd=123;");
+            //optionsBuilder.UseSqlServer("Server=DESKTOP-VU62QDF\SQLSERVERMS; Database=ONLY7BlogProjectDB; Uid=sa; Pwd=123;");
+            //optionsBuilder.UseSqlServer("Server=JUBATUSX; Database=ONLY7BlogProjectDB; Uid=sa; Pwd=123;");
+            //optionsBuilder.UseSqlServer("Server=G*RKEM; Database=ONLY7BlogProjectDB; Uid=sa; Pwd=123;");
         }
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItem { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductOrder> ProductOrders { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
 
