732bfcad48d7fb44b338a981e393bc58db1bb0df
ilkay <arslanilkay06@gmail.com>
2024-04-01 18:09:31 +0300

    Ui katmanında login ve register işlemlerine başlandı

---
+++ ECommerce.UI/Controllers/AccountController.cs
@@ -0,0 +1,89 @@
+﻿using ECommerce.Application.Models.DTOs.UserDto;
+using ECommerce.Application.Services.AppUserService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Policy;
+
+namespace ECommerce.UI.Controllers
+{
+    [Authorize]
+    [AutoValidateAntiforgeryToken]
+    public class AccountController : Controller
+    {
+        private readonly IAppUserService _userService;
+
+        public AccountController(IAppUserService userService)
+        {
+            _userService = userService;
+        }
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+
+        [HttpPost, AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterDto registerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(registerDto);
+            }
+
+            var result = await _userService.Register(registerDto);
+            return RedirectToAction("Index", "Home");
+        }
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl = "/")
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewData["returnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost, AllowAnonymous]
+        public async Task<IActionResult> Login(LoginDto loginDto, string returnUrl = "/")
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userService.Login(loginDto);
+                if (result.Succeeded)
+                {
+                    if (await _userService.UserInRole(loginDto.UserName, "Admin"))
+                    {
+                        return RedirectToAction("Index", "Home", new { area = "Admin" });
+                    }
+
+                    else if (await _userService.UserInRole(loginDto.UserName, "Member"))
+                    {
+                        return RedirectToAction("Index", "Home", new { area = "Member" });
+                    }
+
+                    return RedirectToLocal(returnUrl);
+                }
+                ModelState.AddModelError("", "Hatalı Giriş İşlemi");
+            }
+            return View(loginDto);
+        }
+        private IActionResult RedirectToLocal(string returnUrl = "/")
+        {
+            if (Url.IsLocalUrl(returnUrl))
+                return Redirect(returnUrl);
+
+            else
+                return RedirectToAction("Index", "Home");
+        }
+        public async Task<IActionResult> Logout()
+        {
+            await _userService.LogOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- ECommerce.UI/Program.cs
+++ ECommerce.UI/Program.cs
@@ -1,3 +1,7 @@
+using ECommerce.Domain.Entities;
+using ECommerce.Infrastructure.Context;
+using Microsoft.AspNetCore.Identity;
+
 namespace ECommerce.UI
 {
     public class Program
@@ -8,6 +12,23 @@ namespace ECommerce.UI
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<AppDbContext>();
+
+            builder.Services.AddIdentity<AppUser, IdentityRole>(opt =>
+            {
+                opt.SignIn.RequireConfirmedEmail = false;
+                opt.SignIn.RequireConfirmedPhoneNumber = false;
+                opt.SignIn.RequireConfirmedAccount = false;
+
+                opt.User.RequireUniqueEmail = false;
+
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequiredLength = 3;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+            }).AddEntityFrameworkStores<AppDbContext>()
+              .AddDefaultTokenProviders();
 
             var app = builder.Build();
 
