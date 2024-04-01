3cd36e8738988bf20e7081615811474bb3205425
Gorkem Hosca <gorkemhosca@gmail.com>
2024-04-01 11:34:45 +0300

    Product Entity sine prop eklendi.

---
+++ ECommerce.Infrastructure/Context/AppDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Infrastructure.Context
+{
+    public class AppDBContext : IdentityDbContext
+    {
+        public AppDBContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Server=DESKTOP-16FKK09; Database=ECommerceDb; Uid=sa; Pwd=123;");
+            //optionsBuilder.UseSqlServer("Server=G™RKEMH; Database=ECommerceDb; Uid=sa; Pwd=123;");
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            
+            base.OnModelCreating(builder);
+        }
+    }
+}
