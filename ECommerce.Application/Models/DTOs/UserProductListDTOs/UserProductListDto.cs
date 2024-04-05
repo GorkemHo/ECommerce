07eb8c3e356944958a42b33165aff46e896a109c
atalayozkn <atalayozkan16899@gmail.com>
2024-04-05 17:08:09 +0300

    product filtreleme hazırlandı

--- ECommerce.Application/Models/DTOs/UserProductListDTOs/UserProductListDto.cs
+++ ECommerce.Application/Models/DTOs/UserProductListDTOs/UserProductListDto.cs
@@ -1,5 +1,6 @@
 ﻿using ECommerce.Application.Models.DTOs.OrderDTOs;
 using ECommerce.Application.Models.DTOs.UserDto;
+using ECommerce.Application.Models.VMs.CategoryVMs;
 using ECommerce.Application.Models.VMs.OrderVMs;
 using ECommerce.Application.Models.VMs.ProductOrderVMs;
 using ECommerce.Application.Models.VMs.ProductVMs;
@@ -16,8 +17,10 @@ namespace ECommerce.Application.Models.DTOs.UserProductListDTOs
     {
         public List<UpdateProfileDto>? AppUsers {  get; set; }
         public List<ProductVm>? Products {  get; set; }
+        public List<CategoryVm>? categoryVms {  get; set; }
         public List<OrderVm>? Orders {  get; set; } 
         public CreateOrderDto? CreateOrder { get; set; }
         public UpdateOrderDto? updateOrder { get; set; }
+        public string CategoryName { get; set; }
     }
 }
